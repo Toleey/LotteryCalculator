4dcae4b19dac6247ec570befae5dcf34b2240002
Toleey <Toleey@outlook.com>
2021-06-23 23:15:30 +0800

    First Commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+window.onload = function (){
+    var limitRedBall = document.getElementById("limitRedBall");
+    var numRedBall = document.getElementById("numRedBall");
+    var limitBlueBall = document.getElementById("limitBlueBall");
+    var numBlueBall = document.getElementById("numBlueBall");
+
+    var getMoney = document.getElementById("getMoney");
+    var redBallValue = document.getElementById("redBallValue");
+    var blueBallValue = document.getElementById("blueBallValue");
+
+
+    getMoney.onclick = function (){
+        var redBall = randomNum(limitRedBall.value,numRedBall.value);
+        var blueBall = randomNum(limitBlueBall.value,numBlueBall.value);
+        alert(redBall)
+        alert(blueBall)
+     
+
+    }
+}
+    function randomNum(maxNum,limit){
+        var numArray = new Array();
+        for (var i = 0;i < limit;i++){
+            numArray[i] = Math.floor(Math.random()*maxNum);
+        }
+        return numArray;
+    }
